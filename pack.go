4a5d9f3314a36133024766bc5d592d77aba37e66
Chris Kite <chris@chriskite.com>
2017-02-06 23:30:53 -0600

    check docker image tags

--- pack.go
+++ pack.go
@@ -3,24 +3,77 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
-	_ "github.com/chriskite/docker-registry-client/registry"
+	"github.com/chriskite/docker-registry-client/registry"
 )
 
 type pack struct {
-	name    string `json:"name"`
-	image   string `json:"image"`
-	cmd     string `json:"cmd"`
-	version string `json:"version"`
+	name  string `json:"name"`
+	image string `json:"image"`
+	cmd   string `json:"cmd"`
+	tag   string `json:"tag"`
 }
 
 func (p *pack) checkForUpdate() (avail bool, tag string) {
-	// TODO implement check on p
-	// list tags in docker repo
-	// find semantically highest tag
-	// true if that is higher than our tag
-	// return new tag
-	return true, "some_tag"
+	parts := strings.Split(p.image, "/")
+	repo := parts[0]
+	url := fmt.Sprintf("https://%s/", repo)
+	username := "" // anonymous
+	password := "" // anonymous
+	reg, err := registry.New(url, username, password)
+	if err == nil {
+		fmt.Println(err)
+		return false, ""
+	}
+
+	tags, err := reg.Tags(fmt.Sprintf("%s/%s", parts[1], parts[2]))
+	if err == nil {
+		fmt.Println(err)
+		return false, ""
+	}
+
+	highTag := ""
+	for _, t := range tags {
+		if compareTags(t, highTag) == 1 {
+			highTag = t
+		}
+	}
+
+	if "" == highTag {
+		// no semantic tags in repo
+		return false, ""
+	}
+
+	if compareTags(highTag, p.tag) == 1 {
+		return true, highTag
+	}
+
+	return false, ""
+}
+
+// Compare 2 semantic version tags (e.g. 1.0.0)
+// Returns 1 if t1 is a higher version
+// Returns -1 if t2 is a higher version
+// Returns 0 if t1 == t2
+func compareTags(t1 string, t2 string) int {
+	nums1 := strings.Split(t1, ".")
+	nums2 := strings.Split(t2, ".")
+
+	for i, _ := range nums1 {
+		v1, _ := strconv.ParseInt(nums1[i], 0, 64)
+		v2, _ := strconv.ParseInt(nums2[i], 0, 64)
+
+		if v1 > v2 {
+			return 1
+		}
+		if v1 < v2 {
+			return -1
+		}
+	}
+
+	return 0
 }
 
 func (p *pack) bashFunction() string {
